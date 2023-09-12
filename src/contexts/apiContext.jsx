76cc4f87ceac163c3dfc7a2ecba17a1dc5af7cb5
jgrey2550 <jgrey2550@gmail.com>
2023-09-12 13:41:56 -0700

    test gcp api

--- src/contexts/apiContext.jsx
+++ src/contexts/apiContext.jsx
@@ -3,9 +3,10 @@ import React from "react";
 const ApiContext = React.createContext();
 
 const ApiProvider = ({ children }) => {
-    const apiUrl = 'https://typle-api.vercel.app';
-    //switch for deploy https://typle-api.vercel.app
+    const apiUrl = 'https://our-bruin-398600.uc.r.appspot.com';
+    //switch for vercel api https://typle-api.vercel.app
     //switch for local http://localhost:5000
+    //switch for gcp api https://our-bruin-398600.uc.r.appspot.com
 
     return (
         <ApiContext.Provider value={{ apiUrl }}>
