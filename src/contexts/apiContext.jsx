4f3b2ed7df198f10746e6a78298151dcdb0ca773
jgrey2550 <jgrey2550@gmail.com>
2023-09-13 17:40:20 -0700

    change battle f api

--- src/contexts/apiContext.jsx
+++ src/contexts/apiContext.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ApiContext = React.createContext();
 
 const ApiProvider = ({ children }) => {
-    const apiUrl = 'http://localhost:5000';
+    const apiUrl = 'https://our-bruin-398600.uc.r.appspot.com';
     //switch for vercel api https://typle-api.vercel.app
     //switch for local http://localhost:5000
     //switch for gcp api https://our-bruin-398600.uc.r.appspot.com
